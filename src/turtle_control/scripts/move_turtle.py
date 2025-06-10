8ee0e5cd0f9355b8fb848a0c1303111fa9a1e4ac
Roberto Gil <Rgilh@hotmail.com>
2025-06-10 11:48:36 -0700

    la tortuga se mueve en linea recta y gira a los 2 segundos

--- src/turtle_control/scripts/move_turtle.py
+++ src/turtle_control/scripts/move_turtle.py
@@ -6,12 +6,25 @@ def mover_tortuga():
     rospy.init_node('mover_tortuga', anonymous=False)
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(2)  # 2 Hz
+   
+    cmd = Twist()  
+    tiempo_actual=rospy.get_time()
+    tiempo_anterior=tiempo_actual
+    
 
-    cmd = Twist()
-    cmd.linear.x = 1.0    # velocidad hacia adelante
-    cmd.angular.z = 0.5   # velocidad de giro
 
     while not rospy.is_shutdown():
+        
+        tiempo_actual=rospy.get_time()
+        if tiempo_actual-tiempo_anterior>4:
+            cmd.linear.x = 1.0    # velocidad hacia adelante
+
+            cmd.angular.z = 1.0   # velocidad de giro  
+            tiempo_anterior=tiempo_actual    
+        elif tiempo_actual-tiempo_anterior>2:
+            cmd.linear.x = 1.0    # velocidad hacia adelante
+            cmd.angular.z = 0.0   # velocidad de giro  
+        
         pub.publish(cmd)
         rate.sleep()
 
